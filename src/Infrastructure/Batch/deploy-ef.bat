91b0d5860f3aeb4eb11254db8c934e1ee309cf0e
Edgar Flores Palma <eflorespalma@hotmail.com>
2018-11-18 18:13:49 -0500

    Se realizó cambios al batch.

--- src/Infrastructure/Batch/deploy-ef.bat
+++ src/Infrastructure/Batch/deploy-ef.bat
@@ -12,6 +12,7 @@ set PathToEfDll="not-found"
 
 set PathToEfDll_Option1=%PathToNuGetPackages_Fallback1%\microsoft.entityframeworkcore.tools.dotnet\2.0.3\tools\netcoreapp2.1\ef.dll
 set PathToEfDll_Option2=%PathToNuGetPackages_Fallback2%\microsoft.entityframeworkcore.tools.dotnet\2.0.3\tools\netcoreapp2.1\ef.dll
+set PathToEfDll_Option5=%PathToNuGetPackages_Fallback2%\microsoft.entityframeworkcore.tools\2.1.1\tools\netcoreapp2.0\any\ef.dll
 
 set PathToEfDll_Option3=%PathToNuGetPackages%\microsoft.entityframeworkcore.tools\2.0.3\tools\netcoreapp2.1\ef.dll
 set PathToEfDll_Option4=ef.dll
@@ -49,6 +50,15 @@ if %PathToEfDll% EQU "not-found" (
       set PathToEfDll=%PathToEfDll_Option3%
   ) else (
     ECHO ef.dll not found at %PathToEfDll_Option3%      
+  )
+)
+
+if %PathToEfDll% EQU "not-found" (
+  if exist %PathToEfDll_Option5% (
+      ECHO found ef.dll at %PathToEfDll_Option5%
+      set PathToEfDll=%PathToEfDll_Option5%
+  ) else (
+    ECHO ef.dll not found at %PathToEfDll_Option5%      
   )
 )
 
