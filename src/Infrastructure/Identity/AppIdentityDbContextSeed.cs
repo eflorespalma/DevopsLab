819f15d278f2d5d71454a3a4eb39ed40a99f56ca
Edgar Flores Palma <eflorespalma@hotmail.com>
2018-11-19 17:36:40 -0500

    Default User.

--- src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
+++ src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -8,7 +8,7 @@ namespace Microsoft.eShopWeb.Infrastructure.Identity
         public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
         {
             var defaultUser = new ApplicationUser { UserName = "eflorespalma@microsoft.com", Email = "eflorespalma@microsoft.com" };
-            await userManager.CreateAsync(defaultUser, "Pass@word1");
+            await userManager.CreateAsync(defaultUser, "P@ssw0rd.321");
         }
     }
 }
